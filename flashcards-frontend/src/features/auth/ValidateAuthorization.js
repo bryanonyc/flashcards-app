8836071d3697484f32c2bed6fa7782f3e60997fa
bryanonyc <bryanonyc@yahoo.com>
2025-02-09 23:13:54 -0500

    Remove console logs

--- flashcards-frontend/src/features/auth/ValidateAuthorization.js
+++ flashcards-frontend/src/features/auth/ValidateAuthorization.js
@@ -6,20 +6,16 @@ import { Outlet } from 'react-router-dom';
 
 const ValidateAuthorization = () => {
     const accessToken = getAccessToken();
-    console.log('at', accessToken);
     const effectRan = useRef(false);
     const [isRefreshError, setIsRefreshError] = useState(false);
 
     useEffect(() => {
-        console.log('useEffect -> effectRan.current', effectRan.current);
-        if (effectRan.current) {
+        if (effectRan.current || process.env.NODE_ENV !== 'development') {
             const refreshAccessToken = async () => {
-                console.log('useEffect -> fetch');
                 const response = await fetch(REFRESH_URL, {
                     credentials: 'include',
                 });
                 const data = await response.json();
-                console.log('useEffect -> data', data);
                 if (response.ok) {
                     setAccessToken(data.accessToken);
                 } else {
@@ -28,20 +24,17 @@ const ValidateAuthorization = () => {
             };
 
             if (!accessToken) {
-                console.log('refreshing');
                 refreshAccessToken();
             }
         }
 
         return () => {
-            console.log('set effect ran to true');
             effectRan.current = true;
         };
     });
 
     let content;
 
-    console.log('isRefreshError', isRefreshError);
     if (isRefreshError) {
         //token: no
         content = (
