c266e35aede991333369de7c07d991a70c1ae827
bryanonyc <bryanonyc@yahoo.com>
2025-02-10 11:49:44 -0500

    Styling tweaks to the StudyResults component

--- flashcards-frontend/src/features/components/StudyResults.js
+++ flashcards-frontend/src/features/components/StudyResults.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Alert } from 'antd';
+import { Alert, Flex } from 'antd';
 
 const StudyResults = ({ correct, incorrect, total }) => {
     const successPercentage = ((correct / total) * 100).toFixed(2);
     const errorPercentage = ((incorrect / total) * 100).toFixed(2);
     return (
         <>
-            <Alert
-                message={`Success Percent: ${successPercentage}%`}
-                type='success'
-                showIcon
-            />
-            <Alert
-                message={`Error Percent: ${errorPercentage}%`}
-                type='error'
-                showIcon
-            />
+            <Flex gap='small'>
+                <Alert
+                    message={`${successPercentage}%`}
+                    type='success'
+                    showIcon
+                    style={{ width: '50%' }}
+                />
+                <Alert
+                    message={`${errorPercentage}%`}
+                    type='error'
+                    showIcon
+                    style={{ width: '50%' }}
+                />
+            </Flex>
         </>
     );
 };
